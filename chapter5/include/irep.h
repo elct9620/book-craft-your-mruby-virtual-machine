eabaae5fc3b197028f514f2f25f8ca83c28fecce
蒼時弦也 <elct9620@frost.tw>
2021-10-08 22:05:13 +0800

    Implement irep_read_header

---
+++ chapter5/include/irep.h
@@ -0,0 +1,17 @@
+#ifndef MVM_IREP_H
+#define MVM_IREP_H
+
+#include<stdint.h>
+#include<stdlib.h>
+#include "utils.h"
+
+typedef struct irep_header {
+  uint32_t size;
+  uint16_t nlocals;
+  uint16_t nregs;
+  uint16_t nirep;
+} irep_header;
+
+irep_header* irep_read_header(const uint8_t* ptr, uint8_t* len);
+
+#endif
--- chapter5/test/test_main.c
+++ chapter5/test/test_main.c
@@ -1,6 +1,7 @@
 #include<unity.h>
 
 #include "test_utils.h"
+#include "test_irep.h"
 
 int main() {
   UNITY_BEGIN();
@@ -9,6 +10,9 @@ int main() {
   RUN_TEST(test_bin_to_uint8);
   RUN_TEST(test_bin_to_uint8);
   RUN_TEST(test_bin_to_uint8);
+
+  // IREP
+  RUN_TEST(test_irep_read_header);
 
   return UNITY_END();
 }

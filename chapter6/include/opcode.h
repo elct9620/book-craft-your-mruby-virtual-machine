87290dac95e96a11d632ed6da73f35909cc89529
蒼時弦也 <elct9620@frost.tw>
2021-10-21 21:35:38 +0800

    Add OPCode

---
+++ chapter6/include/opcode.h
@@ -0,0 +1,18 @@
+#ifndef MVM_OPCODE_H
+#define MVM_OPCODE_H
+
+enum {
+  OP_NOP,
+  OP_LOADI_0 = 6,
+  OP_LOADI_1,
+  OP_LOADI_2,
+  OP_LOADI_3,
+  OP_LOADI_4,
+  OP_LOADI_5,
+  OP_LOADI_6,
+  OP_LOADI_7,
+  OP_RETURN = 55,
+  OP_ADDI = 60,
+};
+
+#endif
